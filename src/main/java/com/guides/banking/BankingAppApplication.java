950d668dfeb412d3ad1c16b6dfec0e87e821c3c6
Varsha <varshachindhe01@gmail.com>
2024-08-23 16:49:32 +0530

    second commit

--- src/main/java/com/guides/banking/BankingAppApplication.java
+++ src/main/java/com/guides/banking/BankingAppApplication.java
@@ -9,5 +9,6 @@ public class BankingAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankingAppApplication.class, args);
 		System.out.println("Hello world,How are you!");
+		System.out.println("new changes");
 	}
 }
